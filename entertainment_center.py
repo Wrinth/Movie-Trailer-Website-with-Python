af8c68f61dc79f29fa8fbcf4cb62d6de68d28558
Ho Yeung Lai <johnl4112@gmail.com>
2017-05-29 13:32:19 -0700

    Add files via upload
    Updated entertainment_center.py, fixed string length

--- entertainment_center.py
+++ entertainment_center.py
@@ -13,28 +13,31 @@ You can add new favorite movie following this path:
 toy_story = media.Movie("Toy Story",
                         "A story of a boy and his toys that come to life",
                         "1h 21m",
-                        "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
-                        "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+                        "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg", # NOQA
+                        "https://www.youtube.com/watch?v=KYz2wyBy3kc") # NOQA
 avatar = media.Movie("Avatar",
                      "A marine on an alien planet",
                      "2h 42m",
-                     "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
-                     "https://www.youtube.com/watch?v=5PSNL1qE6VY")
+                     "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg", # NOQA
+                     "https://www.youtube.com/watch?v=5PSNL1qE6VY") # NOQA
 forrest_gump = media.Movie("Forrest Gump",
                            "The story of a man and his epic journey through life",
                            "2h 22m",
-                           "https://upload.wikimedia.org/wikipedia/en/6/67/Forrest_Gump_poster.jpg",
-                           "https://www.youtube.com/watch?v=uPIEn0M8su0")
+                           "https://upload.wikimedia.org/wikipedia/en/6/67/Forrest_Gump_poster.jpg", # NOQA
+                           "https://www.youtube.com/watch?v=uPIEn0M8su0") # NOQA
 school_of_rock = media.Movie("School of Rock",
-                             "Overly enthusiastic guitarist Dewey Finn (Jack Black) gets thrown out of his bar band and finds himself in desperate need of work.",
+                             "Overly enthusiastic guitarist Dewey Finn (Jack \
+                              Black) gets thrown out of his bar band and finds \
+                              himself in desperate need of work.",
                              "1h 49m",
-                             "https://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg",
-                             "https://www.youtube.com/watch?v=3PsUJFEBC74")
+                             "https://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg", # NOQA
+                             "https://www.youtube.com/watch?v=3PsUJFEBC74") # NOQA
 ratatouille = media.Movie("Ratatouille",
-                          "Remy (Patton Oswalt), a resident of Paris, appreciates good food and has quite a sophisticated palate.",
+                          "Remy (Patton Oswalt), a resident of Paris, \
+                           appreciates good food and has quite a sophisticated palate.", 
                           "1h 55m",
-                          "https://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg",
-                          "https://www.youtube.com/watch?v=1yKqLNnxGZw")
+                          "https://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg", # NOQA
+                          "https://www.youtube.com/watch?v=1yKqLNnxGZw") # NOQA
 
 """
 PLace my favorite movies into a the movies list
